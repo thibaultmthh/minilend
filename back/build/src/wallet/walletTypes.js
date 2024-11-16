a62524db56ea4af2b16c475402d8d0f63d81d115
yoanslvy <97676023+yoanslvy@users.noreply.github.com>
2024-11-16 19:33:23 +0700

    clean

--- back/build/src/wallet/walletTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WalletVersion = void 0;
-var WalletVersion;
-(function (WalletVersion) {
-    WalletVersion["V1"] = "v1.0";
-    WalletVersion["V2"] = "v2.0";
-})(WalletVersion || (exports.WalletVersion = WalletVersion = {}));
-//# sourceMappingURL=walletTypes.js.map
